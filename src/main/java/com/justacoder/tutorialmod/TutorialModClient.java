e5bce6b6298451861c7fd6ef7e8b5d4266d532e2
-JustACoder <josiah.seyller@gmail.com>
2025-05-21 10:03:26 -0400

    Initial Commit

---
+++ src/main/java/com/justacoder/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.justacoder.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

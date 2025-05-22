000da3680fddc96734e8e11656f083b3dd83a934
-JustACoder <josiah.seyller@gmail.com>
2025-05-22 09:12:40 -0400

    Added Block of Pink Garnet

--- src/main/java/com/justacoder/tutorialmod/TutorialMod.java
+++ src/main/java/com/justacoder/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.justacoder.tutorialmod;
 
+import com.justacoder.tutorialmod.block.ModBlocks;
 import com.justacoder.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
